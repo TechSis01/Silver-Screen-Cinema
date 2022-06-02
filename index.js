b7d5325097f28404b4865659508bc22c9c2136b9
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-06-02 01:15:06 +0100

    i added a pause and play javascript function for the home page

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const loader = document.querySelector('#preloader')
-window.addEventListener('load', function(){
+window.addEventListener('load', function() {
     loader.style.display = 'none'
 })
+
+var vid = document.getElementById("myVideo");
+
+function playVid() {
+    vid.play();
+}
+
+function pauseVid() {
+    vid.pause();
+}
