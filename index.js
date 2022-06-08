c1342f31cbac8ff5dafd48d5f58db5a9e02bd806
TechSis01 <97944210+TechSis01@users.noreply.github.com>
2022-06-08 14:33:12 +0100

    added a new navbar to homepage,fixed schedule images

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ function playVid() {
 function pauseVid() {
     vid.pause();
 }
+const hamburger = document.querySelector('.hamburger')
+
+//navbar toggle
+    hamburger.addEventListener('click',()=>{
+        let nav = document.querySelector('.nav-link')
+        nav.classList.toggle('nav-active')
+    })
+
